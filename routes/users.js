d6c0d8412b4b86847576b687d58c77837a71d7f0
anastafj197 <frankie.anastasia@gmail.com>
2018-09-18 23:06:51 -0400

    routes

---
+++ routes/users.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+var multer = require('multer');
+var upload = multer({dest: './uploads'});
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Register' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login' });
+});
+
+router.post('/register', upload.single('profileimage'), function(req, res, next) {
+  console.log(req.body.name);
+});
+
+module.exports = router;
